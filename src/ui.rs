fed03b59444f56aa91dd7f3ab9ab1a081d078d72
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2023-05-30 00:00:57 +0200

    wip

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,5 @@
 use tui::{
-    backend::{Backend},
+    backend::{Backend, CrosstermBackend},
     layout::{Alignment, Rect, Layout, Direction, Constraint},
     style::{Color, Style},
     widgets::{Block, BorderType, Borders, Paragraph},
@@ -24,18 +24,29 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
         .border_type(BorderType::Rounded)
         .border_style(Style::default().fg(Color::Cyan));
 
-    let layout_vertical = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(vec![
-            Constraint::Percentage(48),
-            Constraint::Percentage(50),
-            Constraint::Percentage(2),
-            ])
-        .split(main_block.inner(area));
+    let center_block_rarea = centered_rect(50, 50, main_block.inner(area), frame);
+    center_block_render(frame, main_block.inner(center_block_rarea));
 
-                                                                    
-    let title_block = Block::default();                                            
-    let title_text = vec![
+    frame.render_widget(main_block, area);
+}
+
+/// helper function to create a centered rect using up certain percentage of the available rect `r`
+pub fn centered_rect<B: Backend>(percent_x: u16, percent_y: u16, r: Rect, f:  &mut Frame<'_, B>) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_y) / 2),
+                Constraint::Percentage(percent_y),
+                Constraint::Percentage((100 - percent_y) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(r);
+
+        // TOP BLOCK
+        let title_block = Block::default();
+        let title_text = vec![
         Spans::from(Span::raw(r"______                _                  ______         _        ")),
         Spans::from(Span::raw(r"| ___ \              | |                 |  ___|       | |       ")),
         Spans::from(Span::raw(r"| |_/ /__ _ _ __   __| | ___  _ __ ___   | |_ __ _  ___| |_ ___  ")),
@@ -43,25 +54,44 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
         Spans::from(Span::raw(r"| |\ \ (_| | | | | (_| | (_) | | | | | | | || (_| | (__| |_\__ \ ")),
         Spans::from(Span::raw(r"\_| \_\__,_|_| |_|\__,_|\___/|_| |_| |_| \_| \__,_|\___|\__|___/"))];
 
-    let title_paragraph = Paragraph::new(title_text)
-                    .block(title_block)
-                    .alignment(Alignment::Center);
-       
-    frame.render_widget(title_paragraph, layout_vertical[0]);
+        let title_paragraph = Paragraph::new(title_text)
+        .block(title_block)
+        .alignment(Alignment::Center);
 
+        f.render_widget(title_paragraph, popup_layout[0]);
+    
 
+        // BOTTOM BLOCK
+        let footer_block = Block::default();
 
+        let footer_text = Paragraph::new(format!(" Press N to get a new random fact
+                                                                  \n Press 'Esc', 'q' or Ctrl + c to escape the program"))
+                                .block(footer_block.clone())
+                                .alignment(Alignment::Center);
 
-    let footer_block = Block::default();
+        f.render_widget(footer_text, popup_layout[2]);
+    
 
-    let footer_text = Paragraph::new(format!("Press 'Esc', 'q' or Ctrl + c to escape the program"))
-                            .block(footer_block.clone())
-                            .alignment(Alignment::Center);
+        Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(
+                [
+                    Constraint::Percentage((100 - percent_x) / 2),
+                    Constraint::Percentage(percent_x),
+                    Constraint::Percentage((100 - percent_x) / 2),
+                ]
+                .as_ref(),
+            )
+            .split(popup_layout[1])[1]
+}
 
-    frame.render_widget(footer_text, layout_vertical[2]);
- 
+fn center_block_render<B: Backend>(f: &mut Frame<'_, B>, r: Rect) {
+    let layout_vertical = Layout::default()
+    .direction(Direction::Vertical)
+    .constraints(vec![Constraint::Percentage(100)])
+    .split(r);
 
-    // fill_main_block(frame, main_block.inner(area));
+    let center_block = Block::default().borders(Borders::all());
+    f.render_widget(center_block, layout_vertical[0])
 
-    frame.render_widget(main_block, area);
 }
